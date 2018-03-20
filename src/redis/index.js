0cb7bd8f1139f48a60d3319bf5316fd3db6e79ca
erlangxk <simon.xikun@gmail.com>
2018-03-20 12:43:36 +0800

    test pub/sub

--- src/redis/index.js
+++ src/redis/index.js
@@ -1,5 +1,6 @@
 const redis = require('redis');
-const client = redis.createClient({
+
+const retry = {
     retry_strategy: function (opts) {
         if (opts.error && opts.error.code === 'ECONNREFUSED') {
             return new Error('The server refused the connection');
@@ -12,10 +13,13 @@ const client = redis.createClient({
         }
         return Math.min(options.attempt * 100, 3000);
     }
-});
+};
+const client = redis.createClient(retry);
+const client2 = redis.createClient(retry);
+
 
 client.on('error', function (err) {
-    console.log("redis client error");
+    console.log("redis client error",err);
 });
 
 function channel(gameId) {
@@ -23,7 +27,7 @@ function channel(gameId) {
 }
 
 function publish(client, gameId, data) {
-    client.push(channel(gameId), JSON.stringify(data));
+    client.publish(channel(gameId), JSON.stringify(data));
 }
 
 function test() {
@@ -33,8 +37,20 @@ function test() {
         } else {
             console.log(reply);
         }
-        client.quit();
     });
+
+    client.on("message",function(channel,message){
+        console.log(message);
+        client.quit();
+        client2.quit();
+    })
+
+    client.on("subscribe",function(channel, count){
+        publish(client2, 1, { a: 3 });    
+        publish(client2, 1, { a: 4 });    
+    });
+
+    client.subscribe(channel(1));
 }
 
 test();
